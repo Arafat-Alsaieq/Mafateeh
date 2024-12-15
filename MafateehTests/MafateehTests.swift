198d3c5c93ad91ef2e1b9e8c4afcde424727d8ca
Razan66 <raldughaythir24@twq.idserve.net>
2024-12-15 10:43:11 +0300

    Initial Commit

---
+++ MafateehTests/MafateehTests.swift
@@ -0,0 +1,17 @@
+//
+//  MafateehTests.swift
+//  MafateehTests
+//
+//  Created by Arafat on 15/12/2024.
+//
+
+import Testing
+@testable import Mafateeh
+
+struct MafateehTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
