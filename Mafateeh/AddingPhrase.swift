b442e7079825627cdee732c470e74d02c113e5eb
deveLujain <lujainashahri@gmail.com>
2024-12-16 11:38:51 +0300

    f

---
+++ Mafateeh/AddingPhrase.swift
@@ -0,0 +1,18 @@
+//
+//  AddingPhrase.swift
+//  Mafateeh
+//
+//  Created by Lujain sh on 16/12/2024.
+//
+
+import SwiftUI
+
+struct AddingPhrase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingPhrase()
+}
