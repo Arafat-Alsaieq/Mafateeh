8b4556085d0e22a249339c423c8230c0bdee3a36
deveLujain <lujainashahri@gmail.com>
2024-12-16 11:43:34 +0300

    xzzx

--- Mafateeh/AddingPhrase.swift
+++ Mafateeh/AddingPhrase.swift
@@ -1,5 +1,3 @@
-//
-//  AddingPhrase.swift
 //  Mafateeh
 //
 //  Created by Lujain sh on 16/12/2024.
@@ -8,11 +6,50 @@
 import SwiftUI
 
 struct AddingPhrase: View {
+    @State private var showSheet = false
+    @State private var newPhrase: String = "" // To store the user input
+    @State private var savedPhrases: [String] = [] // To store the saved phrases
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("ADD PHRASE") {
+                showSheet.toggle()
+            }
+            
+            List(savedPhrases, id: \.self) { phrase in
+                Text(phrase)
+            }
+        }
+        .sheet(isPresented: $showSheet) {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button("Save") {
+                        if !newPhrase.isEmpty {
+                            savedPhrases.append(newPhrase)
+                            newPhrase = ""
+                            showSheet = false
+                        }
+                    }
+                    .padding()
+                    .disabled(newPhrase.isEmpty) // Disable the button if no text
+                }
+                
+                // text field
+                TextField("Enter your phrase...", text: $newPhrase)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Spacer()
+            }
+            .presentationDetents([.height(200), .medium, .large])
+        }
+        .padding()
     }
 }
 
-#Preview {
-    AddingPhrase()
+struct AddingPhrase_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingPhrase()
+    }
 }
