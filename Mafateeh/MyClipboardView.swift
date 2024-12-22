aa9f33e71dd47a983da167706f7d8ec97eec69f2
maryambsq <maryamx399@gmail.com>
2024-12-22 12:55:22 +0300

    added MyClipboard View

---
+++ Mafateeh/MyClipboardView.swift
@@ -0,0 +1,18 @@
+//
+//  MyClipboardView.swift
+//  Mafateeh
+//
+//  Created by Maryam Amer Bin Siddique on 21/06/1446 AH.
+//
+
+import SwiftUI
+
+struct MyClipboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyClipboardView()
+}
