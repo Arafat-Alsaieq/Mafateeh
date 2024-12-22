5316b29cfd294bc1566a5ac2c5ac467a57b29bb0
maryambsq <maryamx399@gmail.com>
2024-12-22 12:42:58 +0300

    updated

---
+++ Enlarged Keys English/EnglishMainView.swift
@@ -0,0 +1,18 @@
+//
+//  EnglishMainView.swift
+//  Enlarged Keys English
+//
+//  Created by Maryam Amer Bin Siddique on 21/06/1446 AH.
+//
+
+import SwiftUI
+
+struct EnglishMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnglishMainView()
+}
