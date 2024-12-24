7ccf50feb43acd31aa22b229ce3aa76f6a143a39
Arafat-Alsaieq <a.h.b.alsaieq@gmail.com>
2024-12-24 11:53:26 +0300

    Update

---
+++ Mafateeh/AppFeautures_Home.swift
@@ -0,0 +1,18 @@
+//
+//  AppFeautures_Home.swift
+//  Mafateeh
+//
+//  Created by Arafat on 24/12/2024.
+//
+
+import SwiftUI
+
+struct AppFeautures_Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppFeautures_Home()
+}
