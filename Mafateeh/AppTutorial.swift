8d433dfa09ca055c9581e97f49d9e06692b66eae
Arafat-Alsaieq <a.h.b.alsaieq@gmail.com>
2024-12-25 10:33:49 +0300

    Navigation

---
+++ Mafateeh/AppTutorial.swift
@@ -0,0 +1,18 @@
+//
+//  AppTutorial.swift
+//  Mafateeh
+//
+//  Created by Arafat on 25/12/2024.
+//
+
+import SwiftUI
+
+struct AppTutorial: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppTutorial()
+}
