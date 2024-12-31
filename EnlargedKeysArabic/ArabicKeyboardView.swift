a67ffb36976b1f6d7d1d57ce5f41e5a0a0632721
maryambsq <maryamx399@gmail.com>
2024-12-31 21:27:24 +0300

    added functioning Arabic Keyboard.

---
+++ EnlargedKeysArabic/ArabicKeyboardView.swift
@@ -0,0 +1,18 @@
+//
+//  ArabicKeyboardView.swift
+//  EnlargedKeysArabic
+//
+//  Created by Maryam Amer Bin Siddique on 30/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ArabicKeyboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArabicKeyboardView()
+}
---
+++ EnlargedKeysArabic/ArabicNumbers.swift
@@ -0,0 +1,18 @@
+//
+//  ArabicNumbers.swift
+//  EnlargedKeysArabic
+//
+//  Created by Maryam Amer Bin Siddique on 30/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ArabicNumbers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArabicNumbers()
+}
